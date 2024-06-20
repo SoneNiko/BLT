50fb294e5f3fbac81ba0a267c3d7442f71750f4e
Michael Rittmeister <michael@rittmeister.in>
2024-06-20 20:45:07 +0200

    Update to Kotlin 2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,8 @@ import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
 
 plugins {
-    kotlin("multiplatform") version "1.9.23"
-    kotlin("plugin.serialization") version "1.9.23"
+    kotlin("multiplatform") version "2.0.0"
+    kotlin("plugin.serialization") version "2.0.0"
 }
 
 group = "com.sonefall"
@@ -32,6 +32,8 @@ kotlin {
         binaries.executable {
             baseName = "BLT-$targetName"
             entryPoint = "com.sonefall.blt.main"
+
+            runTask?.args(project.findProperty("runArgs")?.toString()?.split(" ") ?: emptyList<String>())
         }
     }
 
@@ -39,13 +41,12 @@ kotlin {
         commonMain {
             dependencies {
                 implementation("io.ktor:ktor-client-core:2.3.11")
-                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
-                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.0")
                 implementation("com.fleeksoft.ksoup:ksoup:0.1.2")
                 implementation("com.github.ajalt.clikt:clikt:4.4.0")
-                implementation("org.jetbrains.kotlinx:kotlinx-io-core:0.3.5")
+                implementation("org.jetbrains.kotlinx:kotlinx-io-core:0.4.0")
                 implementation("io.github.oshai:kotlin-logging:6.0.9")
-
             }
         }
         named("jvmMain") {
